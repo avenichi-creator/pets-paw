dff9fa6bb44609f29dd640dbbedef671c0a3d50e
Viacheslav Falchenko <viacheslav.falchenko@nixsolutions.com>
2023-09-14 20:49:13 +0300

    test commit

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,9 +2,7 @@ import React from 'react';
 import './index.scss';
 
 function App() {
-  return (
-    <div className="app"></div>
-  )
+	return <div className="app"></div>;
 }
 
 export default App;
