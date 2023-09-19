4965dc36c9afed9ba672c8b0d0328bf8e666b295
Viacheslav Falchenko <viacheslav.falchenko@nixsolutions.com>
2023-09-19 12:54:50 +0300

    feat: add routing
    Add mock pages, layout component. Small import order adjustments.

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import { Routing } from 'pages';
 import './index.scss';
 
 function App() {
-	return <div className="app"></div>;
+	return (
+		<div className="app">
+			<Routing />
+		</div>
+	);
 }
 
 export default App;
---
+++ src/pages/error-404-page/index.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export function Error404Page() {
+	return <>404</>;
+}
